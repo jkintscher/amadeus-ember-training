e5a17f2dd73374710d167f9433eb0b89e3a7f07f
Ali Abdelkafi <ali.abdelkafi@amadeus.com>
2014-11-28 17:40:48 +0100

    Use ember data

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 App = Ember.Application.create({
-	LOG_TRANSITIONS: true
+  LOG_TRANSITIONS: true
 });
 
-App.Router.map(function() {
-  this.resource('posts', function() {
-  	this.route('new');
-  
-		this.resource('post', { path: '/post/:post_id' }, function() {
-	    this.route('edit');
-	  });
-	});
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+App.Router.map(function () {
+  this.resource('posts', function () {
+    this.route('new');
+
+    this.resource('post', {path: '/post/:post_id'}, function () {
+      this.route('edit');
+    });
+  });
 });
 
 App.IndexRoute = Ember.Route.extend({
-	redirect: function() {
+  redirect: function () {
     this.transitionTo('posts');
   }
 });
 
 App.PostsRoute = Ember.Route.extend({
-  model: function() {
-    return posts;
+  model: function () {
+    return this.store.find('post');
   },
   afterModel: function(posts, transition) {
     if (posts.get('length') >= 1) {
@@ -30,63 +32,67 @@ App.PostsRoute = Ember.Route.extend({
 });
 
 App.PostRoute = Ember.Route.extend({
-  model: function(params) {
-    return posts.findBy('id', params.post_id);
-  }
-});
-
-App.PostsNewRoute = Ember.Route.extend({
-  model: function() {
-    return posts;
+  model: function (params) {
+    return this.store.find('post', params.post_id);
   }
 });
 
 App.PostsNewController = Ember.Controller.extend({
-	actions: {
-		addNewPost: function(title, excerpt, body) {
-			var newPost = {
-				id: Date.now(),
-				title: title,
-				author: "Amadeus",
-				date: new Date(),
-				excerpt: excerpt,
-				body: body,
-				comments: []
-			};
-			posts.push(newPost);
-			this.set('model', posts);
-		}
-	}
+  actions: {
+    addNewPost: function (title, excerpt, body) {
+      var newPost = {
+        title: title,
+        author: "Amadeus",
+        date: new Date(),
+        excerpt: excerpt,
+        body: body,
+        comments: []
+      };
+
+      var record = this.store.createRecord('post', newPost);
+      record.save();
+    }
+  }
 });
 
-var posts = [{
-	id: '1',
-	title: "Happy birthday Yaohua",
-	author: "Amadeus",
-	date: new Date('2014-10-13'),
-	excerpt: "Say happy birthday to Yaohua !",
-	body: "Today is the birthday of our dear collegue Yaohua, let's celebrate it !",
-	comments: [
-		{visiter: "Muriel", comment:"Salut Yaohua"}, {visiter: "Santi", comment:"Ola Yaohua"}
-	]
-}, {
-	id: '2',
-	title: "Happy birthday Ali",
-	author: "Amadeus",
-	date: new Date('2015-01-12'),
-	excerpt: "Say happy birthday to Ali !",
-	body: "Today is the birthday of our dear collegue Ali, let's celebrate it !",
-	comments: [
-		{visiter: "Muriel", comment:"Salut Ali"}, {visiter: "Santi", comment:"Ola Ali"}
-	]
-}, {
-	id: '3',
-	title: "Happy birthday Joschka",
-	author: "Amadeus",
-	date: new Date('2014-12-24'),
-	excerpt: "Say happy birthday to Joschka !",
-	body: "Today is the birthday of our dear collegue Joschka, let's celebrate it !",
-	comments: [
-		{visiter: "Muriel", comment:"Salut Joschka"}, {visiter: "Santi", comment:"Ola Joschka"}
-	]
-}];
+App.Post = DS.Model.extend({
+  title: DS.attr('string'),
+  author: DS.attr('string'),
+  date: DS.attr('date'),
+  excerpt: DS.attr('string'),
+  body: DS.attr('string'),
+  comments: DS.attr()
+});
+
+App.Post.FIXTURES = [
+  {
+    id: 1,
+    title: "Happy birthday Yaohua",
+    author: "Amadeus",
+    date: new Date('2014-10-13'),
+    excerpt: "Say happy birthday to Yaohua !",
+    body: "Today is the birthday of our dear collegue Yaohua, let's celebrate it !",
+    comments: [
+      {visiter: "Muriel", comment: "Salut Yaohua"}, {visiter: "Santi", comment: "Ola Yaohua"}
+    ]
+  }, {
+    id: 2,
+    title: "Happy birthday Ali",
+    author: "Amadeus",
+    date: new Date('2015-01-12'),
+    excerpt: "Say happy birthday to Ali !",
+    body: "Today is the birthday of our dear collegue Ali, let's celebrate it !",
+    comments: [
+      {visiter: "Muriel", comment: "Salut Ali"}, {visiter: "Santi", comment: "Ola Ali"}
+    ]
+  }, {
+    id: 3,
+    title: "Happy birthday Joschka",
+    author: "Amadeus",
+    date: new Date('2014-12-24'),
+    excerpt: "Say happy birthday to Joschka !",
+    body: "Today is the birthday of our dear collegue Joschka, let's celebrate it !",
+    comments: [
+      {visiter: "Muriel", comment: "Salut Joschka"}, {visiter: "Santi", comment: "Ola Joschka"}
+    ]
+  }];
