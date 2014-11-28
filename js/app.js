44e98b59bc5bb289044581602ea947212a846ea6
unknown <yliang@NCEL17094.iis.amadeus.net>
2014-11-28 14:16:12 +0100

    Creates basic blog

--- js/app.js
+++ js/app.js
@@ -1,21 +1,37 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+	LOG_TRANSITIONS: true
+});
 
 App.Router.map(function() {
   this.resource('posts', function() {
   	this.route('new');
-  });
-
-  this.resource('post', { path: '/post/:post_id' }, function() {
-    this.route('edit');
-    this.resource('comments', function() {
-      this.route('new');
-    });
-  });
+  
+		this.resource('post', { path: '/post/:post_id' }, function() {
+	    this.route('edit');
+	  });
+	});
 });
 
 App.IndexRoute = Ember.Route.extend({
+	redirect: function() {
+    this.transitionTo('posts');
+  }
+});
+
+App.PostsRoute = Ember.Route.extend({
   model: function() {
     return posts;
+  },
+  afterModel: function(posts, transition) {
+    if (posts.get('length') >= 1) {
+      this.transitionTo('post', posts.get('firstObject'));
+    }
+  }
+});
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.post_id);
   }
 });
 
@@ -23,21 +39,30 @@ var posts = [{
 	id: '1',
 	title: "Happy birthday Yaohua",
 	author: "Amadeus",
-	date: new Date('2014-10-13'),
-	excerpt: "Say happy birthday to Yaohua Liang !",
-	body: "Today is the birthday of our dear collegue Yaohua Liang, let's celebrate it !"
+	date: '2014-10-13',
+	excerpt: "Say happy birthday to Yaohua !",
+	body: "Today is the birthday of our dear collegue Yaohua, let's celebrate it !",
+	comments: [
+		{visiter: "Muriel", comment:"Salut Yaohua"}, {visiter: "Santi", comment:"Ola Yaohua"}
+	]
 }, {
 	id: '2',
 	title: "Happy birthday Ali",
 	author: "Amadeus",
-	date: new Date('2014-11-11'),
+	date: '2014-11-11',
 	excerpt: "Say happy birthday to Ali !",
-	body: "Today is the birthday of our dear collegue Ali, let's celebrate it !"
+	body: "Today is the birthday of our dear collegue Ali, let's celebrate it !",
+	comments: [
+		{visiter: "Muriel", comment:"Salut Ali"}, {visiter: "Santi", comment:"Ola Ali"}
+	]
 }, {
 	id: '3',
 	title: "Happy birthday Joschka",
 	author: "Amadeus",
-	date: new Date('2014-12-24'),
+	date: '2014-12-24',
 	excerpt: "Say happy birthday to Joschka !",
-	body: "Today is the birthday of our dear collegue Joschka, let's celebrate it !"
+	body: "Today is the birthday of our dear collegue Joschka, let's celebrate it !",
+	comments: [
+		{visiter: "Muriel", comment:"Salut Joschka"}, {visiter: "Santi", comment:"Ola Joschka"}
+	]
 }];
